97ae578a20c2b66ca7935b9bc60db7471d74c369
tarikochuery <ttarikechuery@gmail.com>
2023-01-25 20:59:33 -0300

    refactor: Adicionadas mais algumas configurações de reset

--- src/assets/style/GlobalStyle.jsx
+++ src/assets/style/GlobalStyle.jsx
@@ -1,6 +1,13 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+* {
+	padding: 0;
+	margin: 0;
+	box-sizing: border-box;
+	text-decoration: none;
+}
+
 html, body, div, span, applet, object, iframe,
 h1, h2, h3, h4, h5, h6, p, blockquote, pre,
 a, abbr, acronym, address, big, cite, code,
