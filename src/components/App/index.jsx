51dc5505a0ebaac2c582e8618a2214df18e9b332
Dickson Pinheiro <dicksonpinheiro4@gmail.com>
2023-01-25 21:47:00 -0300

    corrigindo importações

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,11 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import GlobalStyle from "../../assets/style/GlobalStyle";
-import Car from "../pages/car";
-import Home from "../../pages/Home";
-import Login from "../../pages/Login";
-import Products from "../../pages/Products";
-import Registration from "../../pages/Registration";
-import Router from "../../Routes/Router";
+import Router from "../../Routes/Router"
 
 function App() {
   return (
