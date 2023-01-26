ba7e95ada0b19feafb9cae6082660f23f863ab3f
breno-aredes <breno_mg10@hotmail.com>
2023-01-26 12:01:58 -0300

    layout dos produtos feitos

---
+++ src/assets/style/buttonStyled.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const ButtonStyled = styled.button`
+  width: 150px;
+  height: 46px;
+  border-radius: 5px;
+  background-color: #affe77;
+  border: none;
+  font-family: "Poppins", sans-serif;
+  font-weight: 600;
+  font-size: 20px;
+  line-height: 23.48px;
+  color: #ffffff;
+  cursor: pointer;
+  transition: background 0.3s;
+  &:hover {
+    background-color: #cafe90;
+  }
+`;
