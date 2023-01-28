2c6a64392a3074d16fb6e9f3462f06f7ed8219d3
Dickson Pinheiro <dicksonpinheiro4@gmail.com>
2023-01-28 19:26:17 -0300

    redirecionando para login caso token seja inválido ou inexistente

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,6 @@
-import Cart from "../../components/Cart";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import useApi from "../../hooks/useApi";
 import { FormsSteps } from "../../components/forms/index";
 import Header from "../../components/Header";
 import Products from "../../components/Products";
@@ -6,6 +8,21 @@ import CartProvider from "../../contexts/CartContext";
 import { BodyHome } from "./style";
 
 export default function Home() {
+  const api = useApi(localStorage.getItem("token"))
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    async function validToken() {
+      const result = await api.verifyToken(localStorage.getItem("token"))
+      console.log(result.validationToken.data.valid)
+      if (!result.validationToken.data.valid) {
+        navigate("/")
+      }
+      return
+    }
+    validToken()
+  }, [])
+
   return (
     <CartProvider>
       <Header />
